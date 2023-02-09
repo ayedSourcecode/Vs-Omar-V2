d052f5481cfdce65b66942b7f832432878b989af
ayedFNFmakerSource code <123101780+ayedSourcecode@users.noreply.github.com>
2023-02-09 22:40:53 +0100

    Update Discord.hx

--- source/Discord.hx
+++ source/Discord.hx
@@ -42,10 +42,10 @@ class DiscordClient
 	static function onReady()
 	{
 		DiscordRpc.presence({
-			details: "In the Menus",
+			details: "Playing mods Vs Omar",
 			state: null,
 			largeImageKey: 'icon',
-			largeImageText: "Psych Engine"
+			largeImageText: "Vs Omar V2"
 		});
 	}
 
@@ -82,7 +82,7 @@ class DiscordClient
 			details: details,
 			state: state,
 			largeImageKey: 'icon',
-			largeImageText: "Engine Version: " + MainMenuState.psychEngineVersion,
+			largeImageText: "Vs Omar Version: " + MainMenuState.OmarVersion,
 			smallImageKey : smallImageKey,
 			// Obtained times are in milliseconds so they are divided so Discord can use it
 			startTimestamp : Std.int(startTimestamp / 1000),
